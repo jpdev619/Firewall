f0c8bec577ed29141ade17d93a746e2b84867db2
John Philp Villanueva <janpelep@gmail.com>
2023-09-13 22:56:16 +0800

    Push

--- app/Models/FirewallDetail.php
+++ app/Models/FirewallDetail.php
@@ -9,3 +9,20 @@ class FirewallDetail extends Model
 {
     use HasFactory;
 }
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class FirewallDetail extends Model
+{
+    use HasFactory;
+    protected $table = 'firewall_details';
+    protected $primaryKey = 'ip_id';
+
+    public function fwipmany() {
+        return $this->hasMany(FirewallRequest::class,'fw_number','fw_number');
+    }
+}
